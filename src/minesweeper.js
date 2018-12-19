342ea0f218f12de177557e7b1b5f87c4f9aa58f4
Clarissa Gunawan <gunawan.clarissa@hotmail.com>
2018-12-19 10:37:21 +0000

    print board structure

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,16 +1,37 @@
-//PART 1
-const blankLine = '  |   |  ';
+// PART 1
+// const blankLine = '  |   |  ';
 
-console.log('This is what an empty board would look like: ');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+// console.log('This is what an empty board would look like: ');
+// console.log(blankLine);
+// console.log(blankLine);
+// console.log(blankLine);
 
-const guessLine = '1 |   |  ';
-const bombLine = '  | B |  ';
+// const guessLine = '1 |   |  ';
+// const bombLine = '  | B |  ';
 
-console.log('This is what a board with a guess and a bomb on it would look like: ');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+// console.log('This is what a board with a guess and a bomb on it would look like: ');
+// console.log(guessLine);
+// console.log(bombLine);
+// console.log(blankLine);
+
+// PART 2
+let board = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+const printBoard = board => {
+  console.log('Current Board:');
+  for(let i=0; i<board.length; i++){
+    console.log(board[i].join(' | '));
+  }
+}
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
 
